fcef9b7d3cd795bc9fbbee4e14ecb8c639c84e09
Patrice Maldi <patrice.maldi@keolis.com>
2015-03-02 17:05:47 +0100

    import

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fermerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.NotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyIcon.Icon")));
+            this.NotifyIcon.Text = "notifyIcon1";
+            this.NotifyIcon.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fermerToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(112, 26);
+            // 
+            // fermerToolStripMenuItem
+            // 
+            this.fermerToolStripMenuItem.Name = "fermerToolStripMenuItem";
+            this.fermerToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.fermerToolStripMenuItem.Text = "Fermer";
+            this.fermerToolStripMenuItem.Click += new System.EventHandler(this.fermerToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(10, 10);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "~~ Talend2Chouette Server~~";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon NotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fermerToolStripMenuItem;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        TcpListener serverSocket = new TcpListener(9449);
+        TcpClient clientSocket = default(TcpClient);
+        Thread listens;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {   
+                NotifyIcon.Visible = true;
+                NotifyIcon.BalloonTipTitle = "Talend2Chouette Server";
+                NotifyIcon.BalloonTipText = "L'application ce trouve ici !";
+                NotifyIcon.ShowBalloonTip(500);
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                NotifyIcon.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listens = new Thread(new ThreadStart(Connect));
+            listens.IsBackground = true;
+            listens.Start();
+        }
+        private void Connect()
+        {
+            serverSocket.Start();
+            clientSocket = serverSocket.AcceptTcpClient();
+            NetworkStream networkStream = clientSocket.GetStream();
+            byte[] bytesFrom = new byte[10025];
+            networkStream.Read(bytesFrom, 0, Convert.ToInt32(clientSocket.ReceiveBufferSize));
+            string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+            string dataFromClientok = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+            string[] spliter = dataFromClient.Split('$');
+            try {
+                    if (dataFromClientok == "import") // SI c'est import
+                    {
+                        string Purge = spliter[1];
+                        string DateDebut = spliter[2];
+                        string nbJours = spliter[3];
+                        if (Purge == "ImportPurge") //Si y'a purge
+                        {
+                            System.Diagnostics.Process.Start(@"C:\Users\talend\Desktop\Jobs\purge\Purge\Purge_run.bat", null); 
+                            System.Diagnostics.Process.Start(@"C:\Users\talend\Desktop\Jobs\import\Hastus2Chouette\Hastus2Chouette_run.bat", "--context_param DateDebut=" + DateDebut + " --context_param nbJours=" + nbJours);
+                            Byte[] DataAEnvoyer = Encoding.ASCII.GetBytes("ok$");
+                            networkStream.Write(DataAEnvoyer, 0, DataAEnvoyer.Length);
+                            networkStream.Flush();
+                        }
+                        else//Si y'a pas purge
+                        {
+                            System.Diagnostics.Process.Start(@"C:\Users\talend\Desktop\Jobs\import\Hastus2Chouette\Hastus2Chouette_run.bat", "--context_param DateDebut=" + DateDebut + " --context_param nbJours=" + nbJours);
+                            Byte[] DataAEnvoyer = Encoding.ASCII.GetBytes("ok$");
+                            networkStream.Write(DataAEnvoyer, 0, DataAEnvoyer.Length);
+                            networkStream.Flush();
+                        }
+                    }
+                    else //C'est Export
+                    {
+                        string FormatExport = spliter[1];
+                        string NomduFichier = spliter[2];
+                        System.Diagnostics.Process.Start(@"C:\Users\talend\Desktop\Jobs\export\ExportChouetteNeptune\ExportChouetteNeptune_run.bat", "--context_param FormatExport=" + FormatExport + " --context_param NameExport=" + NomduFichier+".zip");
+                        Byte[] DataAEnvoyer = Encoding.ASCII.GetBytes("ok$");
+                        networkStream.Write(DataAEnvoyer, 0, DataAEnvoyer.Length);
+                        networkStream.Flush();
+                    }
+                    serverSocket.Stop();
+                    listens.Abort();
+            }
+            catch (Exception)
+                {
+                    Byte[] DataAEnvoyer = Encoding.ASCII.GetBytes("pb$");
+                    networkStream.Write(DataAEnvoyer, 0, DataAEnvoyer.Length);
+                    networkStream.Flush();
+                }        
+        }
+
+        private void fermerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
